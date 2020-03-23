de11c574a62df7e96726110413e0ed0a6119cb09
tsaiwentage <gzzxcaiwentian@163.com>
2020-03-24 00:10:50 +0800

    return to the train.py good version

--- unet3d/training.py
+++ unet3d/training.py
@@ -75,28 +75,28 @@ def train_model(model, model_file, training_generator, validation_generator, ste
     :param n_epochs: Total number of epochs to train the model.
     :return: 
     """
-    # model.fit_generator(generator=training_generator,
-    #                     steps_per_epoch=steps_per_epoch,
-    #                     epochs=n_epochs,
-    #                     callbacks=get_callbacks(model_file,
-    #                                             initial_learning_rate=initial_learning_rate,
-    #                                             learning_rate_drop=learning_rate_drop,
-    #                                             learning_rate_epochs=learning_rate_epochs,
-    #                                             learning_rate_patience=learning_rate_patience,
-    #                                             early_stopping_patience=early_stopping_patience),
-    #                     validation_data=validation_generator,
-    #                     validation_steps=validation_steps,
-    #                     workers=0)  # 新增，今天最后试一次 3.11晚
-    #                     # use_multiprocessing=True)  # 将两个validation放后，且增加multiprocessing
-
     model.fit_generator(generator=training_generator,
-                    steps_per_epoch=steps_per_epoch,
-                    epochs=n_epochs,
-                    validation_data=validation_generator,
-                    validation_steps=validation_steps,
-                    callbacks=get_callbacks(model_file,
-                                            initial_learning_rate=initial_learning_rate,
-                                            learning_rate_drop=learning_rate_drop,
-                                            learning_rate_epochs=learning_rate_epochs,
-                                            learning_rate_patience=learning_rate_patience,
-                                            early_stopping_patience=early_stopping_patience))
+                        steps_per_epoch=steps_per_epoch,
+                        epochs=n_epochs,
+                        callbacks=get_callbacks(model_file,
+                                                initial_learning_rate=initial_learning_rate,
+                                                learning_rate_drop=learning_rate_drop,
+                                                learning_rate_epochs=learning_rate_epochs,
+                                                learning_rate_patience=learning_rate_patience,
+                                                early_stopping_patience=early_stopping_patience),
+                        validation_data=validation_generator,
+                        validation_steps=validation_steps,
+                        workers=0)  # 新增，今天最后试一次 3.11晚
+                        # use_multiprocessing=True)  # 将两个validation放后，且增加multiprocessing
+
+    # model.fit_generator(generator=training_generator,
+    #                 steps_per_epoch=steps_per_epoch,
+    #                 epochs=n_epochs,
+    #                 validation_data=validation_generator,
+    #                 validation_steps=validation_steps,
+    #                 callbacks=get_callbacks(model_file,
+    #                                         initial_learning_rate=initial_learning_rate,
+    #                                         learning_rate_drop=learning_rate_drop,
+    #                                         learning_rate_epochs=learning_rate_epochs,
+    #                                         learning_rate_patience=learning_rate_patience,
+    #                                         early_stopping_patience=early_stopping_patience))
