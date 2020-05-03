66c5ffae2c104694b79259293de644bd161239a6
tsaiwentage <gzzxcaiwentian@163.com>
2020-05-03 10:59:16 +0800

    Update train_isensee_1.py

--- brats/train_isensee_1.py
+++ brats/train_isensee_1.py
@@ -66,14 +66,14 @@ def fetch_training_data_files(return_subject_ids=False):
 
 
 def main(overwrite=False):
-    # # convert input images into an hdf5 file
-    # # 若有则加载旧数据集，注意，此时image_shape为之前设置的
-    # if overwrite or not os.path.exists(config["data_file"]):
-    #     training_files, subject_ids = fetch_training_data_files(return_subject_ids=True)
-    #
-    #     write_data_to_file(training_files, config["data_file"], image_shape=config["image_shape"],
-    #                        subject_ids=subject_ids)
-    # data_file_opened = open_data_file(config["data_file"])
+    # convert input images into an hdf5 file
+    # 若有则加载旧数据集，注意，此时image_shape为之前设置的
+    if overwrite or not os.path.exists(config["data_file"]):
+        training_files, subject_ids = fetch_training_data_files(return_subject_ids=True)
+
+        write_data_to_file(training_files, config["data_file"], image_shape=config["image_shape"],
+                           subject_ids=subject_ids)
+    data_file_opened = open_data_file(config["data_file"])
 
     # 加载/创建模型文件
     from keras.utils.vis_utils import plot_model
@@ -89,43 +89,43 @@ def main(overwrite=False):
 
         model.summary()
 
-    # # get training and testing generators
-    # # ../unet3d/generator.py
-    # # 创建生成器(generator)，用于后面训练
-    # train_generator, validation_generator, n_train_steps, n_validation_steps = get_training_and_validation_generators(
-    #     data_file_opened,
-    #     batch_size=config["batch_size"],
-    #     data_split=config["validation_split"],
-    #     overwrite=overwrite,
-    #     validation_keys_file=config["validation_file"],
-    #     training_keys_file=config["training_file"],
-    #     n_labels=config["n_labels"],
-    #     labels=config["labels"],
-    #     patch_shape=config["patch_shape"],
-    #     validation_batch_size=config["validation_batch_size"],
-    #     validation_patch_overlap=config["validation_patch_overlap"],
-    #     training_patch_start_offset=config["training_patch_start_offset"],
-    #     permute=config["permute"],
-    #     augment=config["augment"],
-    #     skip_blank=config["skip_blank"],
-    #     augment_flip=config["flip"],
-    #     augment_distortion_factor=config["distort"])
-    #
-    # # run training
-    # # ../unet3d/training.py
-    # # 训练一个keras模型
-    # train_model(model=model,
-    #             model_file=config["model_file"],
-    #             training_generator=train_generator,
-    #             validation_generator=validation_generator,
-    #             steps_per_epoch=n_train_steps,
-    #             validation_steps=n_validation_steps,
-    #             initial_learning_rate=config["initial_learning_rate"],
-    #             learning_rate_drop=config["learning_rate_drop"],
-    #             learning_rate_patience=config["patience"],
-    #             early_stopping_patience=config["early_stop"],
-    #             n_epochs=config["n_epochs"])
-    # data_file_opened.close()
+    # get training and testing generators
+    # ../unet3d/generator.py
+    # 创建生成器(generator)，用于后面训练
+    train_generator, validation_generator, n_train_steps, n_validation_steps = get_training_and_validation_generators(
+        data_file_opened,
+        batch_size=config["batch_size"],
+        data_split=config["validation_split"],
+        overwrite=overwrite,
+        validation_keys_file=config["validation_file"],
+        training_keys_file=config["training_file"],
+        n_labels=config["n_labels"],
+        labels=config["labels"],
+        patch_shape=config["patch_shape"],
+        validation_batch_size=config["validation_batch_size"],
+        validation_patch_overlap=config["validation_patch_overlap"],
+        training_patch_start_offset=config["training_patch_start_offset"],
+        permute=config["permute"],
+        augment=config["augment"],
+        skip_blank=config["skip_blank"],
+        augment_flip=config["flip"],
+        augment_distortion_factor=config["distort"])
+
+    # run training
+    # ../unet3d/training.py
+    # 训练一个keras模型
+    train_model(model=model,
+                model_file=config["model_file"],
+                training_generator=train_generator,
+                validation_generator=validation_generator,
+                steps_per_epoch=n_train_steps,
+                validation_steps=n_validation_steps,
+                initial_learning_rate=config["initial_learning_rate"],
+                learning_rate_drop=config["learning_rate_drop"],
+                learning_rate_patience=config["patience"],
+                early_stopping_patience=config["early_stop"],
+                n_epochs=config["n_epochs"])
+    data_file_opened.close()
 
 
 if __name__ == "__main__":
@@ -133,7 +133,7 @@ if __name__ == "__main__":
     import tensorflow as tf
     import keras.backend.tensorflow_backend as KTF
 
-    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
     gpuconfig = tf.ConfigProto()
     gpuconfig.gpu_options.allow_growth = True  # 不全部占满显存, 按需分配
     sess = tf.Session(config=gpuconfig)
