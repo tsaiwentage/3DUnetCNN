816c24be16c3df223e47d284e3673e4c64a275fd
tsaiwentage <gzzxcaiwentian@163.com>
2020-05-06 10:47:21 +0800

    switch gpu

--- brats/train_isensee_2.py
+++ brats/train_isensee_2.py
@@ -134,7 +134,7 @@ if __name__ == "__main__":
     import tensorflow as tf
     import keras.backend.tensorflow_backend as KTF
 
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
     gpuconfig = tf.ConfigProto()
     gpuconfig.gpu_options.allow_growth = True  # 不全部占满显存, 按需分配
     sess = tf.Session(config=gpuconfig)
